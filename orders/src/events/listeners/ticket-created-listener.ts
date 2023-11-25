f1c90e4f5863061380810ee2c6a79954653ddaf8
perosky168 <ogunbanjopetet10@gmail.com>
2023-11-25 10:45:08 +0100

    added readme

--- orders/src/events/listeners/ticket-created-listener.ts
+++ orders/src/events/listeners/ticket-created-listener.ts
@@ -1,13 +1,13 @@
 import { Message } from "node-nats-streaming";
-import { Subjects, Listener, TicketCreatedEvent } from "@sgstubhub/common";
+import { Subjects, Listener, TicketCreatedEvent } from "@cygnetops/common-v2";
 import { Ticket } from "../../models/ticket";
 import { queueGroupName } from "./queue-group-name";
 
 export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
-  readonly subject = Subjects.TicketCreated;
+  subject: Subjects.TicketCreated = Subjects.TicketCreated;
   queueGroupName = queueGroupName;
 
-  async onMessage(data: TicketCreatedEvent['data'], msg: Message) {
+  async onMessage(data: TicketCreatedEvent["data"], msg: Message) {
     const { id, title, price } = data;
 
     const ticket = Ticket.build({
